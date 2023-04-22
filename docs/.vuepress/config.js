7fc705d184c97c0c02301ba2a7d179d3e009ca0c
tonngw <1336572985@qq.com>
2023-04-22 22:38:39 +0800

    docs: update docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     title: 'AcWing-Helper',
     description: 'AcWing 刷题助手',
+    base: '/acwing-helper/',
     themeConfig: {
         logo: '/assets/img/logo.png',
         nav: require('./nav/zh'),
