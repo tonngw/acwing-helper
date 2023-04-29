71a6100761286f11fd596cd40e1e27ab07d75343
tonngw <1336572985@qq.com>
2023-04-29 19:03:02 +0800

    docs: update docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -28,14 +28,7 @@ module.exports = {
         nextLinks: true,
         prevLinks: true,
 
-         // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-        repo: 'tonngw/acwing-helper',
-        // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
-        // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-        repoLabel: '查看源码',
-
         // 以下为可选的编辑链接选项
-
         // 假如你的文档仓库和项目本身不在一个仓库：
         repo: 'tonngw/acwing-helper',
         // 假如文档不是放在仓库的根目录下：
