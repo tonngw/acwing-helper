756d2d9f318b9169713a84d0af62dd1fd887c4c8
tonngw <1336572985@qq.com>
2023-04-29 19:40:09 +0800

    docs: update docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -28,18 +28,6 @@ module.exports = {
         nextLinks: true,
         prevLinks: true,
 
-        // 以下为可选的编辑链接选项
-        // 假如你的文档仓库和项目本身不在一个仓库：
-        repo: 'tonngw/acwing-helper',
-        // 假如文档不是放在仓库的根目录下：
-        docsDir: 'docs',
-        // 假如文档放在一个特定的分支下：
-        docsBranch: 'master',
-        // 默认是 false, 设置为 true 来启用
-        editLinks: true,
-        // 默认为 "Edit this page"
-        editLinkText: '完善此页面',
-
         smoothScroll: true
     },
     /* 显示代码的行号 */
@@ -47,5 +35,4 @@ module.exports = {
         lineNumbers: true
     },
     evergreen: true,
-    plugins: ['@vuepress/last-updated']
 }
